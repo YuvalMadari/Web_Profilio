975ffd0999fac98c6c119974ef8cb94a72375e94
Yuval Madari <yuvalmadari1@gmail.com>
2025-08-28 11:18:07 +0300

    header comp added without css

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Toolbar } from 'primeng/toolbar';
+import { AvatarModule } from 'primeng/avatar';
+import { SharedModule } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+    standalone: true,
+    imports: [Toolbar, AvatarModule, ButtonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
