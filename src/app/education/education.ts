20cc6ec0cd57e4f4a2e3734876e7a8b8e6c694d4
Yuval Madari <yuvalmadari1@gmail.com>
2025-08-28 11:14:31 +0300

    edu comp added

--- src/app/education/education.ts
+++ src/app/education/education.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabsModule } from 'primeng/tabs';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-education',
-  imports: [],
+    standalone: true,
+    imports: [CommonModule, TabsModule, BadgeModule, AvatarModule],
   templateUrl: './education.html',
   styleUrl: './education.css'
 })
