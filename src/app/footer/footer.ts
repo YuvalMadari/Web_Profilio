c7fa811c93ca0b593982b1336c9313dc3efc2d0a
Yuval Madari <yuvalmadari1@gmail.com>
2025-08-28 11:17:17 +0300

    footer comp added without css

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FocusTrapModule } from 'primeng/focustrap';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { AutoFocusModule } from 'primeng/autofocus';
+
+import { MessageService } from 'primeng/api';
+import { ProgressBar } from 'primeng/progressbar';
+
+
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [FocusTrapModule, ButtonModule, FormsModule, InputTextModule, CheckboxModule, IconFieldModule, InputIconModule, AutoFocusModule, ProgressBar],
   templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  styleUrl: './footer.css',
+  providers: [MessageService]
 })
 export class Footer {
+    name: string = '';
+
+    email: string = '';
+
+    accept: boolean = false;
 
 }
