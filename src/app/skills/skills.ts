ef7e48f4e00409863f9721120bf6da12db2ee92a
Yuval Madari <yuvalmadari1@gmail.com>
2025-08-28 11:20:58 +0300

    skills comp added without css

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,11 +1,131 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ContextMenu } from 'primeng/contextmenu';
+import { MenuItem } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
+import { BadgeModule } from 'primeng/badge';
+
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+    standalone: true,
+    imports: [ContextMenu, CommonModule, Ripple, BadgeModule],
   templateUrl: './skills.html',
   styleUrl: './skills.css'
 })
-export class Skills {
+export class Skills implements OnInit {
+    items: MenuItem[] | undefined;
 
+    @ViewChild('cm') cm!: ContextMenu;
+
+    selectedId!: string;
+
+    data = [
+        {
+            id: '1000',
+            code: 'f230fh0g3',
+            name: 'Bamboo Watch',
+            description: 'Product Description',
+            image: 'bamboo-watch.jpg',
+            price: 65,
+            category: 'Accessories',
+            quantity: 24,
+            inventoryStatus: 'INSTOCK',
+            rating: 5
+        },
+        {
+            id: '1001',
+            code: 'nvklal433',
+            name: 'Black Watch',
+            description: 'Product Description',
+            image: 'black-watch.jpg',
+            price: 72,
+            category: 'Accessories',
+            quantity: 61,
+            inventoryStatus: 'INSTOCK',
+            rating: 4
+        },
+        {
+            id: '1002',
+            code: 'zz21cz3c1',
+            name: 'Blue Band',
+            description: 'Product Description',
+            image: 'blue-band.jpg',
+            price: 79,
+            category: 'Fitness',
+            quantity: 2,
+            inventoryStatus: 'LOWSTOCK',
+            rating: 3
+        },
+        {
+            id: '1003',
+            code: '244wgerg2',
+            name: 'Blue T-Shirt',
+            description: 'Product Description',
+            image: 'blue-t-shirt.jpg',
+            price: 29,
+            category: 'Clothing',
+            quantity: 25,
+            inventoryStatus: 'INSTOCK',
+            rating: 5
+        },
+        {
+            id: '1004',
+            code: 'h456wer53',
+            name: 'Bracelet',
+            description: 'Product Description',
+            image: 'bracelet.jpg',
+            price: 15,
+            category: 'Accessories',
+            quantity: 73,
+            inventoryStatus: 'INSTOCK',
+            rating: 4
+        }
+    ];
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Favorite',
+                icon: 'pi pi-star',
+                shortcut: '⌘+D'
+            },
+            {
+                label: 'Add',
+                icon: 'pi pi-shopping-cart',
+                shortcut: '⌘+A'
+            },
+            {
+                separator: true
+            },
+            {
+                label: 'Share',
+                icon: 'pi pi-share-alt',
+                items: [
+                    {
+                        label: 'Whatsapp',
+                        icon: 'pi pi-whatsapp',
+                        badge: '2'
+                    },
+                    {
+                        label: 'Instagram',
+                        icon: 'pi pi-instagram',
+                        badge: '3'
+                    }
+                ]
+            }
+        ];
+    }
+
+    onContextMenu(event: MouseEvent) {
+        this.cm.target = event.currentTarget as HTMLElement;
+        this.cm.show(event);
+    }
+
+
+    onHide() {
+        this.selectedId = '';
+
+    }
 }
+
