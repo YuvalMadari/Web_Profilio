a33777331e4f81bdf7d2adf778c20866b37c08f2
Yuval Madari <yuvalmadari1@gmail.com>
2025-08-28 11:19:04 +0300

    projects comp added without css

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+    standalone: true,
+    imports: [CardModule, ButtonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.css'
 })
