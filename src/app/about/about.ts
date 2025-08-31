a4198307b1325842a24bd8efaaa35258b4f387b1
Yuval Madari <yuvalmadari1@gmail.com>
2025-08-31 12:25:31 +0300

    about section + favicon.ico

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +8,31 @@ import { Component } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
-export class About {
+export class About implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  roleText = "Engineer. Coder. Problem Solver.";
+  typingSpeed = 100;
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        window.scrollTo(0, 0);
+      }, 0);
+
+      const typedRole = document.getElementById('typed-role');
+      if (typedRole) typedRole.textContent = '';
+      this.typeRole();
+    }
+  }
+
+  typeRole(index = 0) {
+    const typedRole = document.getElementById('typed-role');
+    if (typedRole) {
+      typedRole.textContent = this.roleText.slice(0, index + 1);
+      if (index < this.roleText.length - 1) {
+        setTimeout(() => this.typeRole(index + 1), this.typingSpeed);
+      }
+    }
+  }
 }
